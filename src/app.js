d8c3d31bc584ddc722f59d5551beb4165852edcf
Wimmu <wimmu.isberg@gmail.com>
2024-04-08 14:46:12 +0300

    tehtävä

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import api from './api/index.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/v1', api);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
+});
+
+export default app;
